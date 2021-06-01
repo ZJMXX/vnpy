7eb215b09dc36989ab3d1b78cce901a62c99da15
015599 <zhoujinming@htsc.com>
2021-06-01 11:00:16 +0800

    [Mod] 修改数据下载，新增数据导出

--- examples/vn_trader/data_download.py
+++ examples/vn_trader/data_download.py
@@ -1,6 +1,5 @@
 import time
 from datetime import datetime
-from concurrent.futures import ThreadPoolExecutor
 
 from backtest_entrance.setting import Info
 from vnpy.app.data_manager import DataManagerApp
@@ -8,7 +7,7 @@ from vnpy.app.data_manager.engine import ManagerEngine
 from vnpy.event import EventEngine
 from vnpy.gateway.binance import BinanceGateway
 from vnpy.gateway.binances import BinancesGateway
-from vnpy.trader.constant import Exchange
+from vnpy.trader.constant import Exchange, Interval
 from vnpy.trader.engine import MainEngine
 
 binance_setting = {
@@ -27,27 +26,28 @@ binances_setting = {
     "代理地址": "127.0.0.1",
     "代理端口": 10809,
 }
-symbol_exchange_dict = {'btcusdt': Exchange.BINANCE,
-                        'ethusdt': Exchange.BINANCE,
-                        'bnbusdt': Exchange.BINANCE,
-                        'adausdt': Exchange.BINANCE,
-                        'dogeusdt': Exchange.BINANCE,
-                        'xrpusdt': Exchange.BINANCE,
-                        'bchusdt': Exchange.BINANCE,
-                        'linkusdt': Exchange.BINANCE,
-                        'ltcusdt': Exchange.BINANCE,
-                        'xlmusdt': Exchange.BINANCE,
-                        'etcusdt': Exchange.BINANCE,
-                        'cocosusdt': Exchange.BINANCE,
-                        'thetausdt': Exchange.BINANCE,
-                        'vetusdt': Exchange.BINANCE,
-                        'eosusdt': Exchange.BINANCE,
-                        'maticusdt': Exchange.BINANCE,
-                        'trxusdt': Exchange.BINANCE,
-                        'xmrusdt': Exchange.BINANCE,
-                        'neousdt': Exchange.BINANCE,
-                        'fttusdt': Exchange.BINANCE,
-                        }
+symbol_exchange_dict = {
+    'btcusdt': Exchange.BINANCE,
+    'ethusdt': Exchange.BINANCE,
+    'bnbusdt': Exchange.BINANCE,
+    'adausdt': Exchange.BINANCE,
+    'dogeusdt': Exchange.BINANCE,
+    'xrpusdt': Exchange.BINANCE,
+    'bchusdt': Exchange.BINANCE,
+    'linkusdt': Exchange.BINANCE,
+    'ltcusdt': Exchange.BINANCE,
+    'xlmusdt': Exchange.BINANCE,
+    'etcusdt': Exchange.BINANCE,
+    'cocosusdt': Exchange.BINANCE,
+    'thetausdt': Exchange.BINANCE,
+    'vetusdt': Exchange.BINANCE,
+    'eosusdt': Exchange.BINANCE,
+    'maticusdt': Exchange.BINANCE,
+    'trxusdt': Exchange.BINANCE,
+    'xmrusdt': Exchange.BINANCE,
+    'neousdt': Exchange.BINANCE,
+    'fttusdt': Exchange.BINANCE,
+}
 
 event_engine = EventEngine()
 main_engine = MainEngine(event_engine)
@@ -58,10 +58,8 @@ main_engine.connect(binance_setting, "BINANCE")
 main_engine.connect(binances_setting, "BINANCES")
 
 manager_engine = ManagerEngine(main_engine=main_engine, event_engine=event_engine)
-time.sleep(3)
-thread_pool = ThreadPoolExecutor(max_workers=10)
+time.sleep(5)
 for i in symbol_exchange_dict.items():
-    thread_pool.submit(manager_engine.download_bar_data, symbol=i[0], exchange=i[1], interval='1m',
-                       start=datetime(2000, 1, 1, 0, 0, 0))
-thread_pool.shutdown(wait=True)
-print('数据下载完成')
+    manager_engine.download_bar_data(symbol=i[0], exchange=i[1], interval=Interval.MINUTE.DAILY,
+                                     start=datetime(2000, 1, 1, 0, 0, 0))
+    print(f'数据下载完成：{i[0]}')
---
+++ examples/vn_trader/data_output.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from vnpy.app.data_manager import DataManagerApp
+from vnpy.app.data_manager.engine import ManagerEngine
+from vnpy.event import EventEngine
+from vnpy.gateway.binance import BinanceGateway
+from vnpy.gateway.binances import BinancesGateway
+from vnpy.trader.constant import Exchange, Interval
+from vnpy.trader.engine import MainEngine
+
+symbol_exchange_dict = {
+    'btcusdt': Exchange.BINANCE,
+    # 'ethusdt': Exchange.BINANCE,
+    # 'bnbusdt': Exchange.BINANCE,
+    # 'adausdt': Exchange.BINANCE,
+    # 'dogeusdt': Exchange.BINANCE,
+    # 'xrpusdt': Exchange.BINANCE,
+    # 'bchusdt': Exchange.BINANCE,
+    # 'linkusdt': Exchange.BINANCE,
+    # 'ltcusdt': Exchange.BINANCE,
+    # 'xlmusdt': Exchange.BINANCE,
+    # 'etcusdt': Exchange.BINANCE,
+    # 'cocosusdt': Exchange.BINANCE,
+    # 'thetausdt': Exchange.BINANCE,
+    # 'vetusdt': Exchange.BINANCE,
+    # 'eosusdt': Exchange.BINANCE,
+    # 'maticusdt': Exchange.BINANCE,
+    # 'trxusdt': Exchange.BINANCE,
+    # 'xmrusdt': Exchange.BINANCE,
+    # 'neousdt': Exchange.BINANCE,
+    # 'fttusdt': Exchange.BINANCE,
+}
+
+event_engine = EventEngine()
+main_engine = MainEngine(event_engine)
+main_engine.add_gateway(BinanceGateway)
+main_engine.add_gateway(BinancesGateway)
+main_engine.add_app(DataManagerApp)
+
+manager_engine = ManagerEngine(main_engine=main_engine, event_engine=event_engine)
+path = 'D:/'
+for i in symbol_exchange_dict.items():
+    file = str(i[0])
+    file_path = path+file+'.csv'
+    manager_engine.output_data_to_csv(file_path=file_path, symbol=i[0], exchange=i[1], interval=Interval.DAILY,
+                                      start=datetime(2000, 1, 1, 0, 0, 0), end=datetime(2099, 12, 31))
+    print(f'数据导出到{file_path}完成：{i[0]}')
